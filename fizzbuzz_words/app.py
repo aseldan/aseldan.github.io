14fc9d3fb7b42bc620da3cf3cdf85ca99ffa1e8a
aseldan <85358718+aseldan@users.noreply.github.com>
2021-07-23 00:10:03 +0600

    Add files via upload

--- fizzbuzz_words/app.py
+++ fizzbuzz_words/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -29,3 +29,54 @@ def anagrams(word):
     title="Anagrams"
     return render_template('words.html', title=title, anagrams=anagrams)
 
+def startwith(word, prefix):
+    if len(prefix)>len(word):
+        return False
+    for i in range(0, len(prefix)): ##len returns a number (int) i need to use range in order to compare from spot zero to the max length of the word 
+        if prefix[i]==word[i]:
+            pass  
+        else:
+            return False     
+    return True 
+
+@app.route("/dictionary/<string:prefix>")
+def dictionary(prefix):
+    f=open('words.txt')
+    words=f.read().splitlines()
+    dictionary={}
+    alphabet= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numWords=0
+
+    for letter in alphabet:   
+        newprefix=prefix+letter 
+        if newprefix not in dictionary:
+            dictionary[newprefix]=[]
+        for word in words:
+            if startwith(word, newprefix):
+                dictionary[newprefix].append(word) 
+                numWords=numWords+1
+    
+    title="Dictionary"
+    #print(dictionary)
+    return render_template('dictionary.html', title=title, dictionary=dictionary, prefix=prefix, count=numWords)
+
+@app.route("/dictionary/")
+def landing():
+    f=open('words.txt')
+    words=f.read().splitlines()
+    dictionary={}
+    alphabet= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numWords=0
+
+    for letter in alphabet:   
+        if letter not in dictionary:
+            dictionary[letter]=[]
+        for word in words:
+            if startwith(word, letter):
+                dictionary[letter].append(word) 
+                numWords=numWords+1
+
+    
+    title="Dictionary"
+    subtitle='choose a letter/prefix'
+    return render_template('dictionary.html', title=title, dictionary=dictionary, prefix='', count=numWords, subtitle=subtitle)
