ea79e56ba5e1d157880d42da3a20b0396e00f7e5
aseldan <85358718+aseldan@users.noreply.github.com>
2021-09-16 16:47:31 +0600

    Add files via upload

--- Unit5/app.py
+++ Unit5/app.py
@@ -7,3 +7,10 @@ app = Flask(__name__)
 def hello_world():
     suppliers = database.get_all_suppliers()
     return render_template('index.html', suppliers=suppliers)
+
+@app.route("/suppliers/<int:supplier_id>")
+def products(supplier_id):
+    products = database.get_supplier_products(supplier_id)
+    supplier=database.get_supplier_name(supplier_id)
+    
+    return render_template('products.html', products=products, supplier=supplier) 
